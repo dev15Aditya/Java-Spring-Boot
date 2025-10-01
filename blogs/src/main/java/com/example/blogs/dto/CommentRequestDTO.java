478738043e6c73e31430d048a84da1a149ecf771
Aditya <aditya1115.ar@gmail.com>
2025-10-01 23:27:13 +0530

    added cooment and post service

--- blogs/src/main/java/com/example/blogs/dto/CommentRequestDTO.java
+++ blogs/src/main/java/com/example/blogs/dto/CommentRequestDTO.java
@@ -1,5 +1,10 @@
 package com.example.blogs.dto;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
 public class CommentRequestDTO {
     private String text;
 }
---
+++ blogs/src/main/java/com/example/blogs/service/CommentService.java
@@ -0,0 +1,30 @@
+package com.example.blogs.service;
+
+import org.springframework.stereotype.Service;
+
+import com.example.blogs.config.ResourceNotFoundException;
+import com.example.blogs.dto.CommentRequestDTO;
+import com.example.blogs.model.Comment;
+import com.example.blogs.model.Post;
+import com.example.blogs.repository.CommentRepository;
+import com.example.blogs.repository.PostRepository;
+
+import lombok.AllArgsConstructor;
+
+@Service
+@AllArgsConstructor
+public class CommentService {
+    private final CommentRepository commentRepository;
+    private final PostRepository postRepository;
+
+    public void addComment(Long postId, CommentRequestDTO dto){
+        Post post = postRepository.findById(postId)
+            .orElseThrow(() -> new ResourceNotFoundException("Post not found with id " + postId));
+
+        Comment comment = new Comment();
+        comment.setText(dto.getText());
+        comment.setPost(post);
+
+        commentRepository.save(comment);
+    }
+}
