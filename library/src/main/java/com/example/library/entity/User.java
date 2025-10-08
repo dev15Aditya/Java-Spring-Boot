481403d97b1d6ca8f0fd194892d515841edfd7ea
Aditya <aditya1115.ar@gmail.com>
2025-10-08 07:59:54 +0530

    Added Book and User Entity

---
+++ library/src/main/java/com/example/library/entity/User.java
@@ -0,0 +1,40 @@
+package com.example.library.entity;
+
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name="users")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class User {
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable=false, unique=true)
+    private String username;
+
+    @Column(nullable=false, unique=true)
+    private String email;
+
+    @Column(nullable=false)
+    private String password;
+
+    @OneToMany(mappedBy="owner", cascade=CascadeType.ALL, orphanRemoval=true)
+    private List<Book> books;
+}
