c9648c2fe7cf1d843261219179605a130aea705e
Aditya <aditya1115.ar@gmail.com>
2025-11-20 22:41:19 +0530

    Added exception handler. for different request

---
+++ devLinks/src/main/java/com/devLinks/dto/ErrorResponse.java
@@ -0,0 +1,16 @@
+package com.devLinks.dto;
+
+import java.time.LocalDateTime;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class ErrorResponse {
+    private LocalDateTime timeStamp;
+    private int status;
+    private String error;
+    private String message;
+    private String path;
+}
