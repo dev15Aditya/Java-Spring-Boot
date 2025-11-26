5b74edaa4e34310f87a1580987532011ad134d62
Aditya <aditya1115.ar@gmail.com>
2025-11-26 20:46:17 +0530

    Added validation to AuthRequest and ProjectRequest DTO, for input validation

--- devLinks/src/main/java/com/devLinks/dto/AuthRequest.java
+++ devLinks/src/main/java/com/devLinks/dto/AuthRequest.java
@@ -1,10 +1,14 @@
 package com.devLinks.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
 public class AuthRequest {
+    @Email
     private String email;
+    @Size(min=6)
     private String password;
     private String name;
 }
